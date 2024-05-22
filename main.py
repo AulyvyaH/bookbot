ed848bd4e686ac4bab6cf5799ded963945b2a1ce
AulyvyaH <o.tournois@protonmail.com>
2024-05-22 11:38:20 +0200

    Added word counter, letter counter to main

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    bookpath = "books/frankenstein.txt"
+    text = get_text(bookpath)
+    print(f"--- Begin report of {bookpath} ---")
+    number_of_words = word_counter(text)
+    print(f"{number_of_words} words found in the document\n")
+    letter_dict = count_letters(text)
+    sorted_dict = sorted(letter_dict)
+    for letter in sorted_dict:
+        print(f"The '{letter}' character was found {letter_dict[letter]} times")
+    print("--- End report ---")
+
+
+def count_letters(text):
+    text_lowered = text.lower()
+    letter_dict = {}
+    for letter in text_lowered:
+        if letter.isalpha():
+            if letter in letter_dict:
+                letter_dict[letter] += 1
+            else:
+                letter_dict[letter] = 1
+    return letter_dict
+        
+
+
+def get_text(path):
+     with open(path) as f: #the file is opened using its relative path (relative to where we run the program)
+        return f.read() #the content of the file is retrieved through the read() function and put in a variable
+
+def word_counter(file_contents):
+    words = file_contents.split()
+    return len(words)
 
 main()
-    
